a1d675813ef31ad5cc5288d93c072b9c8e080798
october-03 <october03@o365sen.net>
2022-09-14 12:32:06 +0900

    create apiRequestUrl

---
+++ pages/api/apiRequestUrl.ts
@@ -0,0 +1,25 @@
+export const BASE_URL = {
+  TEAM_MATCH_API: 'https://192.168.100.55:3001'
+}
+
+export const REQUEST_URL: requestUrlType = {
+  GET_TEAM_LIST: `${BASE_URL.TEAM_MATCH_API}/`
+};
+
+export const changeUrlParams = (url: string, paramList: paramListType[]) => {
+  url = REQUEST_URL[url];
+  paramList.forEach((param) => {
+    url = url.replace(`{${param.key}}`, String(param.value));
+  });
+
+  return url;
+};
+
+type paramListType = {
+  key: string | number;
+  value: string | number;
+};
+
+type requestUrlType = {
+  [key: string]: string;
+};
